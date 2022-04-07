7cb5794d8e1a50e68e76e9cc75ee63e36afa8543
Robert Spektor <rspk85@gmail.com>
2022-04-07 20:52:25 +0200

    WIP

---
+++ pages/groups/_byCategory.vue
@@ -0,0 +1,34 @@
+<template>
+  <GroupList :category="category"></GroupList>
+</template>
+
+<script>
+  import GroupList from "@/components/GroupList";
+
+  export default {
+    head() {
+      return {
+        title: "Deutsche Telegram Gruppen und Kanäle | " + this.category,
+        meta: [
+          {
+            hid: "description",
+            name: "Next Ui Kit",
+            content: "Next Ui Kit is the best way",
+          },
+        ],
+      };
+    },
+    components: { GroupList },
+    data() {
+      return {
+        category: ''
+      }
+    },
+    methods: {
+
+    },
+    setup: function() {
+
+    },
+  };
+</script>
---
+++ stores/categories.js
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+
+export const useCategories = defineStore("categories-store", {
+  state: () => {
+    return {
+      categories: [],
+      finish: false,
+    };
+  },
+
+  getters: {
+    getCategories(state) {
+      return state.categories.data;
+    },
+
+    isFinish(state) {
+      return state.finish;
+    },
+  },
+
+  actions: {
+    async fetch() {
+
+      this.finish = false;
+
+      const response = await fetch(
+        "https://api.telegram.de/categories"
+      );
+
+      try {
+
+        this.categories = await response.json();
+
+        this.finish = true;
+
+      } catch (err) {
+        this.categories = [];
+        console.error("Error loading categories:", err);
+        return err;
+      }
+    },
+  },
+});
